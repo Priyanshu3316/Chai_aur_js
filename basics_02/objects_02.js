db7d66c77785777301f6246b7252f5e4227f4475
Priyanshu3316 <priyanshu3316@gmail.com>
2026-07-18 22:56:58 +0530

    Completed Objects Lecture with practical knowledge

--- basics_02/objects_02.js
+++ basics_02/objects_02.js
@@ -23,5 +23,33 @@ obj1 = { 1: "A", 2: "B" };
 obj2 = { 3: "C", 4: "D" };
 
 // const obj3 = {obj1, obj2}
-const obj3 = Object.assign({}, obj1, obj2); // Adding empty obj just as a syntax a good practice
-console.log(obj3);
+// const obj3 = Object.assign({}, obj1, obj2); // Adding empty obj just as a syntax a good practice
+const obj3 = { ...obj1, ...obj2 }; // Adding empty obj just as a syntax a good practice
+// console.log(obj3);
+
+const users = [
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+];
+
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); // can access all keys of tinderUser object
+console.log(Object.values(tinderUser)); // can access all values of tinderUser Values
+console.log(Object.entries(tinderUser)); // give each entry in different array
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
