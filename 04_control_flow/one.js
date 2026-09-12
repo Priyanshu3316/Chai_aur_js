5fd68e55661f44079c891b22821830b5b00c369c
Priyanshu3316 <priyanshu3316@gmail.com>
2026-09-12 14:16:36 +0530

    Commited all files

---
+++ 04_control_flow/one.js
@@ -0,0 +1,30 @@
+// if
+// const isUserloggedIn = true;
+// const temperature = 41;
+
+// if (temperature === 50) {
+//   console.log("less than 50");
+// } else {
+//   console.log("temperature is greater than 50");
+// }
+
+// <, >, <= , >=, =, != , ===
+
+// const balance = 1000;
+
+// if (balance < 500) {
+//   console.log("less than 500");
+// } else if (balance < 750) {
+//   console.log("less than 750");
+// } else if (balance < 900) {
+//   console.log("less than 750");
+// } else {
+//   console.log("less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if (userLoggedIn && debitCard && 2 == 2) {
+  console.log("Allow to buy course");
+}
